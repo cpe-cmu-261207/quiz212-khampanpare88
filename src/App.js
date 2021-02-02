bf4476a8f36ffb5055fa2ff4659deaca75fe36a3
khampanpare88 <59277375+khampanpare88@users.noreply.github.com>
2021-02-02 13:58:49 +0700

    ;-;

--- src/App.js
+++ src/App.js
@@ -14,9 +14,10 @@ function App() {
   return (
     <div class="ml-2">
       {/* Code me please! */}
+      {}
       <div class="mb-4">
         <h3 class="title is-3">ID Counter</h3>
-        <p>YOUR ID HERE</p>
+        <p>620610777</p>
         <button>-</button>
         <button>reset</button>
         <button>+</button>
@@ -32,9 +33,37 @@ function App() {
             <th>Age</th>
           </tr>
           <tr>
+            <td>Kantika Khampan 620610777</td>
+            <td>Female</td>
+            <td>19</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+          <tr>
             <td>Bob</td>
             <td>male</td>
             <td>50</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="table is-bordered mb-3">
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+          </tr>
+          <tr>
+            <td>Alice</td>
+            <td>male</td>
+            <td>20</td>
           </tr>
         </tbody>
       </table>
